bf2b27a947e7f04ec102a5c1bad36a41d6e3cb30
satyam-seth <satyam1998.1998@gmail.com>
2021-11-03 10:19:42 +0530

    add diwali wish page

--- wish/diwali/urls.py
+++ wish/diwali/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.home, name='diwali_home'),
+    path('home/', views.home, name='diwali_home'),
+    path('wish/', views.wish, name='diwali_wish'),
 ]
--- wish/diwali/views.py
+++ wish/diwali/views.py
@@ -3,9 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    if request.method=='GET':
-        return render(request, 'diwali/diwali.html')
-    elif request.method=='POST':
-        name=request.POST.get('name')
-        return render(request, 'diwali/diwali.html', {'name':name})
-        
+    return render(request, 'diwali/home.html')
+    
+def wish(request):
+    name=request.GET.get('name')
+    return render(request, 'diwali/wish.html', {'name':name})
