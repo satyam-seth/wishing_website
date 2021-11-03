1dd2668343c187f03c46e960609979c543467fb2
satyam-seth <satyam1998.1998@gmail.com>
2021-11-03 09:58:14 +0530

    handel diwali form post request

--- wish/diwali/views.py
+++ wish/diwali/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'diwali/home.html')
+    if request.method=='GET':
+        return render(request, 'diwali/diwali.html')
+    elif request.method=='POST':
+        name=request.POST.get('name')
+        return render(request, 'diwali/diwali.html', {'name':name})
+        
